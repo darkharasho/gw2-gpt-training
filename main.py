d11f7962215cfbf0d92a2e3afb02bae5a20f09ab
Michael Stephens <project96@gmail.com>
2023-10-18 16:34:11 -0700

    Finalize training data v1

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ class FineTuner:
         openai.api_key = os.getenv("OPENAI_API_KEY")
 
     @staticmethod
-    def fine_tune(self, type):
+    def fine_tune(self, model_type):
+        match model_type:
+            case "wvw":
+                file_name = "./gw2_gpt_training/training_data/gw2_wvw_training.jsonl"
+
         openai.File.create(
-            file=open("mydata.jsonl", "rb"),
+            file=open(file_name, "rb"),
             purpose='fine-tune'
         )
         openai.FineTuningJob.create(training_file="file-abc123", model="gpt-3.5-turbo")
@@ -17,6 +21,11 @@ class FineTuner:
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    verify_type = True
+    while verify_type:
+        model_type = input(">> Model Type:")
+        if model_type not in ["wvw"]:
+            print("Invalid Model Type.")
+        else:
+            FineTuner().fine_tune(model_type)
+            verify_type = False
