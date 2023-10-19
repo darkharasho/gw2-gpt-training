f0f3877ab3626ef3a760e9c91c2cac3c1f8c1637
Michael Stephens <project96@gmail.com>
2023-10-18 17:12:23 -0700

    Working fine tuner!

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # gw2-gpt-training
+
+## How to use
+#1 install dependencies: `poetry install`
+#2 Copy `.env.sample` to `.env` and put in your api key
+#2 Run `poetry run python main.py`
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
 import openai
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class FineTuner:
@@ -7,23 +10,27 @@ class FineTuner:
         openai.api_key = os.getenv("OPENAI_API_KEY")
 
     @staticmethod
-    def fine_tune(self, model_type):
+    def fine_tune(model_type):
         match model_type:
             case "wvw":
-                file_name = "./gw2_gpt_training/training_data/gw2_wvw_training.jsonl"
-
-        openai.File.create(
+                file_name = "gw2_gpt_training/training_data/gw2_wvw_training.jsonl"
+        print("Creating training file...")
+        training_file = openai.File.create(
             file=open(file_name, "rb"),
             purpose='fine-tune'
         )
-        openai.FineTuningJob.create(training_file="file-abc123", model="gpt-3.5-turbo")
+        print(f"Training file created: {training_file['id']}")
+        print("Creating Fine Tuning job...")
+        openai.FineTuningJob.create(training_file=training_file["id"], model="gpt-3.5-turbo")
+        print("Done!")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     verify_type = True
     while verify_type:
-        model_type = input(">> Model Type:")
+        print(f"Available Options: [wvw]")
+        model_type = input(">> Model Type: ")
         if model_type not in ["wvw"]:
             print("Invalid Model Type.")
         else:
